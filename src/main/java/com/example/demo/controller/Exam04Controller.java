03f8c4af2aecb5ff9a960d0d4cddaa9ad7b9d799
piyoppa <piyoppa@gmail.com>
2021-08-04 00:30:58 +0900

    Exam04完成
    create Userdomain
    
    create UserForm
    
    create Exam04Controller
    
    create exam04html
    
    create exam04-resulthtml
    
    modified html
    
    modified html
    
    modified html

---
+++ src/main/java/com/example/demo/controller/Exam04Controller.java
@@ -0,0 +1,49 @@
+package com.example.demo.controller;
+
+import org.springframework.beans.BeanUtils;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import com.example.demo.domain.User;
+import com.example.demo.form.UserForm;
+
+@Controller
+@RequestMapping("/exam04")
+
+public class Exam04Controller {
+	
+	@ModelAttribute
+	public UserForm setUpForm() {
+		return new UserForm();
+	}
+	
+	@RequestMapping("")
+	public String index() {
+		return "exam04";
+	}
+	
+	@RequestMapping("/create")
+	public String create(@Validated UserForm form, BindingResult result, RedirectAttributes redirectAttributes) {
+		
+		if( result.hasErrors() ) {
+			return index();
+		}
+		
+		User user = new User();
+		BeanUtils.copyProperties(form, user);
+		
+		redirectAttributes.addFlashAttribute("user", user);
+		
+		return "redirect:/exam04/toresult";
+	}
+	
+	@RequestMapping("/toresult")
+	public String toResult() {
+		return "exam04-result";
+	}
+
+}
